5cf99358e8414d60e8fb94b42e6128c1c5eb8fc3
camila <camilaalape23@gmail.com>
2024-07-24 09:22:20 -0500

    actualizacion

--- Billar/src/components/Navigation.jsx
+++ Billar/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export function Navigation(){
     return(
         <div>
-            <h1> Task App</h1>
+            <h1> Task App HOLA</h1>
             <link to="/tasks-create">create task</link>
         </div>
     )
